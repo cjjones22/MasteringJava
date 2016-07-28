378428226031558156d4433b9cf1e85407fcb059
Chris <cjjones22@gmail.com>
2016-07-28 13:10:02 -0700

    Lesson 7 Exercise 1

---
+++ Lesson_7/src/Lesson_7_Ex1.java
@@ -0,0 +1,18 @@
+/*
+ * Lesson 7, Exercise 1
+
+ * Use the Eclipse IDE to create a new java program and output
+ * the following lines to the screen exactly as written below:
+
+ * How much wood could a woodchuck chuck,
+ * if a woodchuck could chuck wood?
+ */
+public class Lesson_7_Ex1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("How much wood could a woodchuck chuck,");
+		System.out.println("if a woodchuck could chuck wood?");
+	}
+
+}
