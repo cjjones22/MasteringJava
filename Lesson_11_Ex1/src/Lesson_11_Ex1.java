9d85b7657654453a49865bb614c65c2e7caeb36d
Chris <cjjones22@gmail.com>
2016-07-29 15:01:02 -0700

    Lesson 11 Exercise 1

---
+++ Lesson_11_Ex1/src/Lesson_11_Ex1.java
@@ -0,0 +1,27 @@
+/*
+ * Lesson 11, Exercise 1
+
+ * Write a program that outputs the following text, formatted as below:
+
+ * Home Town Hamburger Restaurant
+ * Hours: Mon - Fri, 9am - 9pm
+
+ * Lowest prices in town!
+ * Lunch	Dinner
+ * $5.95	$11.95
+ */
+
+public class Lesson_11_Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Home Town Hamburger Restaurant");
+		System.out.println("Hours:\tMon - Fri, 9am - 9pm\n");
+		System.out.println("Lowest prices in town!");
+		System.out.println("Lunch\tDinner");
+		System.out.println("$5.95\t$11.95");
+		
+	}
+
+}
